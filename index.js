f3498549315b89b13dcca0228f78413b6d2711f9
Rumesa zahra syeda <rumesazahrasyeda@gmail.com>
2025-02-11 18:25:01 +0500

    changes 6

--- index.js
+++ index.js
@@ -1,19 +1,7 @@
-function getUserDetails() {
-    let name = prompt("Enter your name:");
-    let age = prompt("Enter your age:");
-    let totalPayment = prompt("Enter total payment:");
-    
-    let contactNumber;
-    if (age < 18) {
-        contactNumber = prompt("Enter father's number:");
-    } else {
-        contactNumber = prompt("Enter personal number:");
-    }
+let a = prompt("What is your order ")
+let b = prompt("What is your full name")
+let c = prompt("What is your Address")
 
-    document.getElementById("name").innerText = name;
-    document.getElementById("age").innerText = age;
-    document.getElementById("totalPayment").innerText = totalPayment;
-    document.getElementById("contact").innerText = contactNumber;
-}
-
-
+document.getElementById("order").innerText ="order : " + a
+document.getElementById("name").innerText ="name : " + b
+document.getElementById("addres").innerText ="addres : " + c
